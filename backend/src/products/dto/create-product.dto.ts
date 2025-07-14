725c68cbff5d40046fa92f9106b362735c14e275
Anthony <anthonyjosue0011@gmail.com>
2025-07-13 22:04:23 -0500

    Actualizacion dto

--- backend/src/products/dto/create-product.dto.ts
+++ backend/src/products/dto/create-product.dto.ts
@@ -1,4 +1,5 @@
 import { IsString, IsNumber, IsPositive, IsOptional, Min } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CreateProductDto {
   @IsString()
@@ -8,10 +9,12 @@ export class CreateProductDto {
   @IsOptional()
   description?: string;
 
+  @Type(() => Number)
   @IsNumber()
   @IsPositive()
   price: number;
 
+  @Type(() => Number)
   @IsNumber()
   @Min(0)
   stock: number;
