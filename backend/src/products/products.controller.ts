717d5e851c79acb7678273999191985be170b7be
Anthony <anthonyjosue0011@gmail.com>
2025-07-13 20:26:37 -0500

    Agregar GET /products/:id

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { Product } from './schemas/product.schema';
+import { Param } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
